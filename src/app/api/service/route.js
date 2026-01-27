a7137d237423630be588a99eb3835f93aa5569d6
Jahid458 <Jahidul898@outlook.com>
2026-01-27 23:28:17 +0600

    next service get booking api added

--- src/app/api/service/route.js
+++ src/app/api/service/route.js
@@ -1,7 +1,19 @@
+import { authOptions } from "@/lib/authOptions";
 import dbConnect, { collectionNameObject } from "@/lib/dbConnect";
+import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 
+export const GET = async (req) => {
+    const session = await getServerSession(authOptions);
+    if(session){
+        const email = session?.user?.email ;
+        const bookindCollections = dbConnect(collectionNameObject.bookingCollection);
+        const result = await bookindCollections.find({email}).toArray();
+        return NextResponse.json(result)
+    }
 
+    return NextResponse.json({})
+}
 
 export const POST = async(req) =>{
     const body = await req.json();
