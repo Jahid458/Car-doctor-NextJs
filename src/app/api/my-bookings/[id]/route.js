d78cea7e8b6bc97b7fddf80d6c7383bd2f2ef361
Jahid458 <Jahidul898@outlook.com>
2026-01-29 18:16:50 +0600

    update exting user

--- src/app/api/my-bookings/[id]/route.js
+++ src/app/api/my-bookings/[id]/route.js
@@ -1,33 +1,60 @@
-import dbConnect, { collectionNameObject } from "@/lib/dbConnect"
+import { authOptions } from "@/lib/authOptions";
+import dbConnect, { collectionNameObject } from "@/lib/dbConnect";
 import { ObjectId } from "mongodb";
+import { getServerSession } from "next-auth";
 import { revalidatePath } from "next/cache";
-import { NextResponse } from "next/server"
+import { NextResponse } from "next/server";
 
- 
+export const GET = async (req, { params }) => {
+  const p = await params;
+  const bookingCollections = dbConnect(collectionNameObject.bookingCollection);
+  const query = { _id: new ObjectId(p.id) };
+  const session = await getServerSession(authOptions);
+  const email = session?.user?.email;
 
-export const GET = async(req,{params}) =>{
+  const singleBooking = await bookingCollections.findOne(query);
+  const isOwnerOk = email == singleBooking?.email;
 
-    const p = await params ;
-    const bookingCollections = dbConnect(collectionNameObject.bookingCollection);
-    const query ={_id: new ObjectId(p.id)}
-    const singleBooking = await bookingCollections.findOne(query);
+  if (isOwnerOk) {
+    return NextResponse.json(singleBooking);
+  } else {
+    return NextResponse.json(
+      { message: "Forbidden GET Actions" },
+      { status: 403 },
+    );
+  }
+};
 
-    return NextResponse.json(singleBooking)
-}
+export const PATCH = async (req, { params }) => {
+  const p = await params;
+  const bookingCollections = dbConnect(collectionNameObject.bookingCollection);
+  const query = { _id: new ObjectId(p.id) };
 
-export const PATCH = async(req, {params}) => {
-    const p = await params ;
-    const bookingCollections = dbConnect(collectionNameObject.bookingCollection);
-    const query = {_id: new ObjectId(p.id)};
+  const session = await getServerSession(authOptions);
+  const email = session?.user?.email;
+  const currentBookingData = await bookingCollections.findOne(query);
 
+  const isOwnerOk = email == currentBookingData?.email;
+
+  if (isOwnerOk) {
     const body = await req.json();
     const filter = {
-        $set: {...body}
-    }
+      $set: { ...body },
+    };
     const option = {
-        upsert: true ,
-    }
-    const updateResponse = await bookingCollections.updateOne(query,filter,option);
-    revalidatePath('/my-bookings')
-    return NextResponse.json(updateResponse)
-}
+      upsert: true,
+    };
+    const updateResponse = await bookingCollections.updateOne(
+      query,
+      filter,
+      option,
+    );
+    revalidatePath("/my-bookings");
+    return NextResponse.json(updateResponse);
+  } else {
+    return NextResponse.json(
+      { message: "Forbidden update Actions" },
+      { status: 403 },
+    );
+  }
+};
